fbf184780492896d7aaacfe144f32e221a563f14
tanabe_taichi <tanabe_taichi@qualiarts.jp>
2020-05-23 02:51:10 +0900

    4.4.2 損失関数の勾配の計算

---
+++ ch4_neural_network_training/ch4_4_2.py
@@ -0,0 +1,50 @@
+import os
+import sys
+sys.path.append(os.pardir)
+
+import numpy as np
+
+from common.functions import softmax, cross_entropy_error
+from common.gradient import numerical_gradient
+
+
+class SimpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2, 3)
+
+    def predict(self, x):
+        print(f"self.W: {self.W}")
+        return np.dot(x, self.W)
+
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
+        return loss
+
+
+if __name__ == '__main__':
+    net = SimpleNet()
+    print(net.W)
+
+    # 入力
+    x = np.array([0.6, 0.9])
+    # 算出したラベル
+    p = net.predict(x)
+    print(p)
+
+    print(np.argmax(p))
+
+    # 正解ラベル
+    t = np.array([0, 0, 1])
+    # 損失関数の計算
+    print(net.loss(x, t))
+
+
+    def f(W):
+        return net.loss(x, t)
+
+    # net.Wの各要素についてのnet.loss(x, t)の偏微分結果が格納
+    # (net.W自体をnumerical_gradientの中で変化させて計算(偏微分))
+    dW = numerical_gradient(f, net.W)
+    print(dW)
