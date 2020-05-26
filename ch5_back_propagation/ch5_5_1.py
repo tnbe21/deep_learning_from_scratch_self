a7393509e72cdb50589ae69e5017e8212ede84a1
tanabe_taichi <tanabe_taichi@qualiarts.jp>
2020-05-26 10:38:26 +0900

    Reluお試し&コメント更新

--- ch5_back_propagation/ch5_5_1.py
+++ ch5_back_propagation/ch5_5_1.py
@@ -5,10 +5,11 @@ class Relu:
         self.mask = None
 
     def forward(self, x):
-        # np.array型のxが入力される想定で、
-        # self.maskはnp.array型xの各要素が0以下かどうかの論理値のnp.array
+        # x: np.array型
+        # self.mask: xと同じ形式の行列で、各要素0以下ならTrue/そうでなければfalse
         self.mask = (x <= 0)
         out = x.copy()
+        # 各要素0以下で0になる
         out[self.mask] = 0
 
         return out
@@ -18,3 +19,10 @@ class Relu:
         dx = dout
 
         return dx
+
+
+if __name__ == '__main__':
+    relu = Relu()
+    x = np.array([[1, -2, 10], [-1, 10, 2]])
+    out = relu.forward(x)
+    print(f"out: {out}")
