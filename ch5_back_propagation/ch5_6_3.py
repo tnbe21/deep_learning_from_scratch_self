d4e47095d51bb85801eebe546767cbd68be58c9a
tanabe_taichi <whpbj809@gmail.com>
2020-06-12 00:57:28 +0900

    ch5_6, 7

---
+++ ch5_back_propagation/ch5_6_3.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+def softmax(a):
+    c = np.max(a)
+    # 決まった数cを入れても変わらない
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    return exp_a / sum_exp_a
+
+
+def cross_entropy_error(y, t):
+    """
+    交差エントロピー誤差出力
+    """
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    # if文なしでこれでいける?
+    # batch_size = y.shape[0] if y.ndim > 1 else 1
+    batch_size = y.shape[0]
+    return -np.sum(t * np.log(y + 1e-7)) / batch_size
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.y = None
+        self.t = None
+        self.loss = None
+
+    def forward(self, x, t):
+        self.y = softmax(x)
+        self.t = t
+        self.loss = cross_entropy_error(self.y, t)
+
+    def backward(self, dout=1):
+        pass
---
+++ ch5_back_propagation/ch5_7_2.py
@@ -0,0 +1,24 @@
+import os, sys
+sys.path.append(os.pardir)
+
+import numpy as np
+
+from common.layers import *
+from common.gradient import numerical_gradient
+from collections import OrderedDict
+
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_sizs, output_size)
+        self.params['b2'] = np.zeros(output_size)
+
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+
+        self.lastLayer = SoftmaxWithLoss()
