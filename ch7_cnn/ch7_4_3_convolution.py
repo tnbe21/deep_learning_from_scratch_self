fddd116bd5c344b4f71c3e2f88aba4d5aa936be7
tanabe_taichi <whpbj809@gmail.com>
2020-10-15 12:23:26 +0900

    CNN Convolutionレイヤ逆伝搬実装

--- ch7_cnn/ch7_4_3_convolution.py
+++ ch7_cnn/ch7_4_3_convolution.py
@@ -24,6 +24,15 @@ class Convolution:
         self.stride = stride
         self.pad = pad
 
+        # 中間データ(backward時に使用)
+        self.x = None
+        self.col = None
+        self.col_W = None
+
+        # 重み・バイアスパラメータの勾配
+        self.dW = None
+        self.db = None
+
     def forward(self, x):
         # フィルタ(W)の
         # * FN: バッチ数
@@ -37,13 +46,31 @@ class Convolution:
         # * H: height
         # * W: width
         N, C, H, W = x.shape
-        out_h = 1 + int((H + 2 * self.pad - FH) / self.stride)
-        out_w = 1 + int((W + 2 * self.pad - FW) / self.stride)
+        out_h = int((H + 2 * self.pad - FH) / self.stride) + 1
+        out_w = int((W + 2 * self.pad - FW) / self.stride) + 1
 
         col = im2col(x, FH, FW, self.stride, self.pad)
         col_W = self.W.reshape(FN, -1).T
 
         out = np.dot(col, col_W) + self.b
+        # transpose()を使い(N, C, H, W)にする
         out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
 
+        self.x = x
+        self.col = col
+        self.col_W = col_W
+
         return out
+
+    def backward(self, dout):
+        FN, C, FH, FW = self.W.shape
+        dout = dout.transpose(0, 2, 3, 1).reshape(-1, FN)
+
+        self.db = np.sum(dout, axis=0)
+        self.dW = np.dot(self.col.T, dout)
+        self.dW = self.dW.transpose(1, 0).reshape(FN, C, FH, FW)
+
+        dcol = np.dot(dout, self.col_W.T)
+        dx = col2im(dcol, self.x.shape, FH, FW, self.stride, self.pad)
+
+        return dx
