644f4ac21bb09c42e5af18c1ecaefe63d6b78a08
tanabe_taichi <whpbj809@gmail.com>
2020-10-15 14:53:32 +0900

    implementing CNN Pooling layer

---
+++ ch7_cnn/ch7_4_4_pooling.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+import numpy as np
+
+sys.path.append(os.pardir)
+
+from common.util import im2col
+
+
+class Pooling:
+    def __init__(self, pool_h, pool_w, stride=1, pad=0):
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.stride = stride
+        self.pad = pad
+
+    def forward(self, x):
+        N, C, H, W = x.shape
+        out_h = int(1 + (H - self.pool_h) / self.stride)
+        out_w = int(1 + (W - self.pool_w) / self.stride)
+
+
